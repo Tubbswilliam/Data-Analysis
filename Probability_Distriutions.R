b52edfbb6f4c0ccdf3ced98e743e67cb28f8f603
Tubbswilliam <williamwambua58@gmail.com>
2021-06-17 12:23:54 +0300

    more probabilities in R

--- Probability_Distriutions.R
+++ Probability_Distriutions.R
@@ -15,3 +15,12 @@ dunif(0,min=0,max=10)
 #Setting the seed will help us to recompute the same random
 #numbers over and over again.
 set.seed(12)
+
+#Normal distribution
+normal_Data<-rnorm(100000,mean=0,sd=1)
+pnorm(q=-1,mean=0,sd=1)
+
+#Binomial distribution
+coinflip<-rbinom(10000,10,0.5)
+hist(coinflip)
+
