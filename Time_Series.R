7e3f1d7230e1f1e812a49f434f26ee07f4f27f2c
Tubbswilliam <williamwambua58@gmail.com>
2021-08-06 17:26:00 +0300

    Time series Analysis ARIMA model

--- Time_Series.R
+++ Time_Series.R
@@ -28,5 +28,22 @@ plot(acf(AirPassengers))#checking the autocorrelation
 #log transformation. Also in order to get the mean to be constant
 #we differentiate the time series.
 plot(diff(log(AirPassengers)))
-
 #From the above, the data is now set for time series modelling
+
+#ARIMA (p,d,q)
+plot(acf(diff(log(AirPassengers))))#To determine q
+plot(pacf(diff(log(AirPassengers))))#To determine p
+
+#d is the amount of times you have done differentiation
+#in this case d is 1 coz i have differentiated once to get
+#the time series stationary.
+
+#Fitting the model
+fit<-arima(log(AirPassengers),c(0,1,1),seasonal = list(order=c(0,1,1),period=12))
+
+#predicting for the next 15 years
+pred<-predict(fit,n.ahead=15*12)
+pred_values=2.718^pred$pred
+pred_values #predicted values
+
+
